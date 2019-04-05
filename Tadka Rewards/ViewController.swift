79c6535a5892d95f5cf514818da0b137cbffc3c0
Nihaal Manesia <nihaal@manesia.org>
2019-04-04 19:02:20 -0500

    Initial Commit

---
+++ Tadka Rewards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tadka Rewards
+//
+//  Created by Nihaal Manesia on 4/4/19.
+//  Copyright © 2019 Nihaal Manesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
